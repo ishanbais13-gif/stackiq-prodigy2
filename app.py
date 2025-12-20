6d4f8a1b8ff69a6b755add48aba4ed3dc9eb607e
ishanbais13-gif <ishanbais13@gmail.com>
2025-12-19 21:31:57 -0500

    Add CORS middleware for localhost access
    Added CORS middleware to allow requests from localhost:3000.

--- app.py
+++ app.py
@@ -97,6 +97,15 @@ def predict(
         raise HTTPException(status_code=e.status_code, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
